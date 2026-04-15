ac815de1cd8cb903582b9bba0c515814db5e5d6f
renz.ayala <renzayala98@gmail.com>
2026-04-15 16:32:48 -0500

    adding trinkets (Warrior only)

--- BossTTD.lua
+++ BossTTD.lua
@@ -1,7 +1,15 @@
 local frame = CreateFrame("Frame")
 local lastHealth, lastTime, lastPrint = 0, 0, 0
 local dmgHistory = {}
-local minDmg = 1000
+--local minDmg = 1000
+
+local cds = {
+    ["Blood Fury"] = { duration = 15, id = 20572 },
+    ["Death Wish"] = { duration = 30, id = 12292 },
+    ["Bloodlust Brooch"] = { duration = 20, id = 29383 },
+    ["Abacus of Violent Odds"] = { duration = 10, id = 28288 },
+    ["Recklessness"] = { duration = 15, id = 1719 }
+}
 
 frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 frame:RegisterEvent("UNIT_HEALTH")
@@ -17,9 +25,14 @@ frame:SetScript("OnEvent", function (self, event, unit)
     elseif event == "UNIT_HEALTH" and unit == "target" then
         local crHealth = UnitHealth("target")
         local crTime = GetTime()
-        local dmg = lastHealth - crHealth
+        --local dmg = lastHealth - crHealth
+        
+        local crHealthMax = UnitHealthMax("target")
+        local crDmg = (crHealth / crHealthMax) * 100
+        local lastDmg = (lastHealth / crHealthMax) * 100
+        local dmg = lastDmg - crDmg 
 
-        if crHealth > 0 and (dmg > minDmg or dmg < 0) then
+        if crHealth > 0 and dmg > 0.1 then
             local timeDiff = crTime - lastTime
 
             if timeDiff > 0 then
@@ -32,13 +45,35 @@ frame:SetScript("OnEvent", function (self, event, unit)
                 local avgDps = sum / #dmgHistory
 
                 if avgDps > 0 then
-                    local ttDie = crHealth / avgDps
+                    --local ttDie = crHealth / avgDps
+                    local ttDie = crDmg / avgDps
                     
                     if ttDie < 600 and (crTime - lastPrint > 1) then
                         local m = math.floor(ttDie / 60)
                         local s = math.floor(ttDie - (m * 60))
                         
                         print(string.format("Dead: %02d:%02d", m, s))
+
+                        for cdName, data in pairs(cds) do
+                            if ttDie <= (data.duration + 2) and ttDie >= (data.duration - 1) then
+                                local cooldownDuration = 0
+                                if cdName == "Bloodlust Brooch" or cdName == "Abacus of Violent Odds" then
+                                    local itemCDInfo = C_Container.GetItemCooldown(data.id)
+                                    if type(itemCDInfo) == "table" then
+                                        cooldownDuration = itemCDInfo.duration or 0
+                                    end
+                                else
+                                    local _, spellCooldown = GetSpellCooldown(data.id)
+                                    cooldownDuration = spellCooldown or 0
+                                end
+
+                                if cooldownDuration == 0 then
+                                    print(string.format("PRESS: %s", cdName))
+                                end
+                                --print(string.format("PRESS: %s", cdName))
+                            end
+                        end
+
                         lastPrint = crTime
                     end
                 end
